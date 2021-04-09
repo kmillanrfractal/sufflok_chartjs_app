f45b6c338ca5c7874b8b6af6ba3798a2d08440f4
kmillanrfractal <kmillanr@fractal-software.com>
2021-04-09 09:15:51 -0400

    added esthetics to chart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import historicalData from '../src/data/historical_data.csv'
 import { Chart, registerables } from 'chart.js';
 
 Chart.register(...registerables);
+Chart.defaults.font.size = 20
+
+
+
+
 function App() {
   useEffect(()=>{
     d3.csv(historicalData).then(makeChart);
@@ -19,18 +24,73 @@ function App() {
       data: {
         labels: year,
         datasets: [
-          {
-            data: weighted_mean
+
+          { label: ["Sqrft Per Year", "pepe"],
+            data: weighted_mean,
+            borderColor : "#fff",
+            borderWidth : "3",
+            hoverBorderColor : "#000",
+            backgroundColor: [
+              "#f38b4a",
+              "#56d798",
+              "#ff8397",
+              "#6970d5",
+              "#acb1ff",
+              "#fffe12",
+              "#ff0012",
+              "#ff00f4",
+              "#1806fb "
+            ],
+            hoverBackgroundColor: [
+              "#b1b2b5"
+            ],
+
           }
         ]
+      },
+      options: {
+        plugins: {
+          responsive: true,
+          legend: {display: true, position: "top", align:"end", fullSize: true, textDirection:'ltr', labels:{textAlign: 'left',padding: 10}}, title:{display: true, text: 'wleiufhoawef'},
+          title: {
+            display: true,
+            text: "BarChart Square Feet by Region",
+            padding: {
+              top: 10,
+              bottom: 20
+            }
+          }
+        },
+        scales: {
+          x: {
+            title: {
+              color: 'black',
+              display: true,
+              text: "Year"
+            }
+          },
+          y:{
+            title: {
+              color: 'black',
+              display: true,
+              text: "Sqrft"
+            }
+          },
+        }
       }
     });
   };
-  console.log('data', data)
   return (
+  <body>
     <div className="App">
-      <canvas id="chart" width="200" height="100" />
+          <h1>Suffolk Charts</h1>
+          <hr></hr>
+          <div class="chart-container">
+            <canvas id="chart" width="100px" height="50px" />
+          </div>
     </div>
+  </body>
+
   );
 }
 
